3e39ee279987b7f9f663a4b7982c4d180b383f62
Facundo Cuba <facundogcuba@gmail.com>
2017-05-03 20:21:03 -0300

    Se reducio el inicio.py y se cargo el esqueleto en cargaDeDatos.py

--- cargaDeDatos.py
+++ cargaDeDatos.py
@@ -1,8 +1,24 @@
 import sqlite3
 
+base = sqlite3.connect('D:\pi.db')
+c = base.cursor()
+
 def carga():
-    base = sqlite3.connect('D:\pi.db')
-    c = base.cursor()
-    c.execute('SELECT * FROM ARTICULOS')
-    a = c.fetchall()
-    print(a)
+    print("Ingrese una opcion para continuar...")
+    print("1 para Cargar Fabricantes")
+    print("2 para Cargar Articulos")
+    print("3 para Salir")
+    inputDeCarga = input()
+       if inputDeCarga == "1":
+            nombre = input("Ingrese Nombre: ")
+            c.execute('INSERT INTO FABRICANTES(NOMBRE) VALUES ("{}");'.format(nombre))
+            a = c.fetchall()
+            carga()
+        elif inputDeCarga == "2":
+            c.execute('SELECT * FROM ARTICULOS')
+            a = c.fetchall()
+            carga()
+        elif inputDeCarga == "3":
+            print("Saliendo...")
+            print("")
+            exit()
--- inicio.py
+++ inicio.py
@@ -10,23 +10,7 @@ def menuInicio():
     inputDelUser = input()
     print("")
     if inputDelUser == "1":
-        print("Ingrese una opcion para continuar...")
-        print("1 para Cargar Fabricantes")
-        print("2 para Cargar Articulos")
-        print("3 para Salir")
-        inputDeCarga = input()
-        if inputDeCarga == "1":
-            cargaDeDatos.carga()
-        elif inputDeCarga == "2":
-            cargaDeDatos.carga()
-        elif inputDeCarga == "3":
-            print("Saliendo...")
-            print("")
-            exit()
-        else:
-            print("Ingrese una opcion valida!")
-            print("")
-            menuInicio()
+        cargaDeDatos.carga()
     elif inputDelUser == "2":
         consultaDeDatos.consulta()
     elif inputDelUser == "3":
